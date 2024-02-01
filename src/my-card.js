cb1c72923f44887889811baad34f6a1fa84dc279
Dylan <61261822+Jalpy-Dev@users.noreply.github.com>
2024-02-01 13:15:32 -0500

    did the stuff

--- src/my-card.js
+++ src/my-card.js
@@ -14,6 +14,10 @@ export class MyCard extends LitElement {
   constructor() {
     super();
     this.title = "My card";
+    this.imageLink = "https://github.com/pennstate.png";
+    this.cardText = "Default card text";
+    this.buttonLink = "#";
+    this.buttonText = "Default";
   }
 
   static get styles() {
@@ -21,16 +25,88 @@ export class MyCard extends LitElement {
       :host {
         display: block;
       }
+
+      .card-wrapper {
+        display: flex;
+      }
+
+      .card {
+        width: 400px;
+        background-color: turquoise;
+        border-style: dashed;
+        border-radius: 10px;
+        margin: 16px;
+      }
+
+      .card p {
+        width: 90%;
+        text-align: center;
+        margin-left: 5%;
+      }
+
+      .card img {
+        width: 350px;
+        padding: 0px 25px;
+      }
+
+      .card h1 {
+        width: 100%;
+        text-align: center;
+      }
+
+      a {
+        text-decoration: none;
+      }
+
+      .btn-wrapper {
+        background-color: orange;
+        padding: 10px;
+        margin: 64px;
+      }
+
+      .btn {
+        background-color: blue;
+        color: white;
+        font-size: 20px;
+        border-radius: 10%;
+        padding: 16px;
+        margin: 2px 75px;
+      }
+
+      .btn:focus,
+      .btn:hover {
+        background-color: green;
+      }
+
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+    <div class="card-wrapper">
+      <div class="card">
+        <h1 class="card-title">${this.title}</h1>
+        <img class="card-image" src=${this.imageLink} />
+        <p>${this.cardText}</p>
+        <div class="btn-wrapper">
+          <a href=${this.buttonLink}>
+            <button class="btn">
+              <p>${this.buttonText}</p>
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+    `;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      imageLink: { type: String },
+      cardText: { type: String },
+      buttonLink: { type: String },
+      buttonText: { type: String },
     };
   }
 }
