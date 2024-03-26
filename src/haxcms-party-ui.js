8bfaa76f0956755e9f0de740d83d60cb83dd534e
Dylan <61261822+Jalpster@users.noreply.github.com>
2024-03-26 13:10:54 -0400

    Update haxcms-party-ui.js

--- src/haxcms-party-ui.js
+++ src/haxcms-party-ui.js
@@ -65,37 +65,81 @@ export class haxcmsPartyUI extends DDD {
     addUser() {
       const inputValue = this.shadowRoot.querySelector('.input-text').value;
 
+      // Check if user already exists
+
+
+      // If they dont exist, continue
       const user = {
         userid: inputValue,
       }
 
       this.partyMembers = [...this.partyMembers, user];
       console.log(this.partyMembers);
-  }
+    }
+
+    targetClicked(e) {
+      // what item bubbled the event
+      console.log(e.target);
+
+      const user = e.target.closest('button').getAttribute('seed');
+
+      const index = this.partyMembers.indexOf(user);
+      this.partyMembers.splice(index)
+
+      this.requestUpdate();
+    }
+
+    cancel() {
+      // Do something eventually
+      // But for now just reset party list to empty
+      this.partyMembers = [];
+    }
+
+    save() {
+      // fireworks
+      this.makeItRain();
+    }
+
+    makeItRain() {
+        import("@lrnwebcomponents/multiple-choice/lib/confetti-container.js").then(
+            (module) => {
+                setTimeout(() => {
+                    this.shadowRoot.querySelector("#confetti").setAttribute("popped", "");
+                }, 0);
+            }
+        );
+    }
 
     render() {
         return html`
-            <div class="frame">
-                <h1 class="frame-title">Add Party Member</h1>
+            <confetti-container id="confetti">
+              <div class="frame">
+                  <h1 class="frame-title">Add Party Member</h1>
 
-                <div class="add-to-party-input">
-                  <input class="input-text" type="text" placeholder="abc123">
-                  <button @click="${this.addUser}">Add</button>
-                </div>
+                  <div class="add-to-party-input">
+                    <input class="input-text" type="text" placeholder="abc123">
+                    <button @click="${this.addUser}">Add</button>
+                  </div>
 
-                <div class="party-members">
-                    <div class="slot">
-                        <rpg-character seed="${this.ownerName}" walking></rpg-character>
-                        <p>${this.ownerName}</p>
-                    </div>
-                    ${this.partyMembers.map((user) => html`
+                  <div class="party-members">
                       <div class="slot">
-                        <button class="delete-user">X</button>
-                        <rpg-character seed=${user.userid} walking @click="${this.targetClicked}"></rpg-character>
-                        <p>${user.userid}</p>
+                          <rpg-character seed="${this.ownerName}" walking></rpg-character>
+                          <p>${this.ownerName}</p>
                       </div>
-                  `)}
-            </div>
+                      ${this.partyMembers.map((user) => html`
+                        <div class="slot">
+                          <button class="delete-user" @click="${this.targetClicked}" seed="${user.userid}">X</button>
+                          <rpg-character seed=${user.userid} walking @click="${this.targetClicked}"></rpg-character>
+                          <p>${user.userid}</p>
+                        </div>
+                    `)}
+                  </div>
+                  <div class="save-cancel-buttons">
+                    <button @click="${this.save}">Save</button>
+                    <button @click="${this.cancel}">Cancel</button>
+                  </div>
+                </div>
+              </confetti-container>
         `;
     }
 
@@ -103,7 +147,7 @@ export class haxcmsPartyUI extends DDD {
     return {
       ...super.properties,
       ownerName: { type: String },
-      partyMembers: { type: Array }
+      partyMembers: { type: Array, reflect: true }
     }
   }
 }
