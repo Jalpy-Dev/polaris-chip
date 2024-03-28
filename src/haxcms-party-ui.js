697541f4cf2790a97a01b1b171e57635e3fb1652
Dylan <61261822+Jalpster@users.noreply.github.com>
2024-03-28 13:09:42 -0400

    good stuff

--- src/haxcms-party-ui.js
+++ src/haxcms-party-ui.js
@@ -19,45 +19,68 @@ export class haxcmsPartyUI extends DDD {
       css`
       :host {
         display: block;
-        background-color: var(--ddd-theme-default-slateMaxLight);
+        background-color: var(--ddd-theme-default-slateLight);
       }
+
+      *, h1 {
+        font-family: "Press Start 2P", system-ui;
+      }
+      
       .frame {
         padding: var(-ddd-spacing-5);
         margin: var(--ddd-spacing-2) var(--ddd-spacing-0);
         text-align: center;
+        height: 100%;
+        padding: 1%;
         /* color: var(--ddd-theme-default-keystoneYellow); */
-      }
-
-      .slot {
-        /* width: var(--m-24); */
-        border-bottom: 2px solid black;
       }
 
       .party-members {
         display: inline-flex;
-        overflow: scroll;
+        overflow-x: scroll;
         width: 90%;
-        height: 20%;
+        height: 50%;
       }
 
       .slot {
         margin: var(--ddd-spacing-2);
         display: flex;
         flex-direction: column;
-        height: 100%;
+        height: 75%;
+        border-bottom: 2px solid black;
+        margin: 0;
+        margin-right: 5%;
+        padding: 0;
+        margin-top: 2%;
       }
 
       .slot p {
         margin: 0;
-        padding: 0;
         color: var(--ddd-theme-default-beaverBlue);
+        font-size: 15px;
       }
 
-      .slot .delete-user {
-        width: 2em;
-        position: relative;
-        left: 80%;
-        top: 10%;
+      .slot button {
+        width: 1rem;
+        height: 1rem;
+        border: 0;
+        border-radius: 50%;
+        background-color: var(--ddd-theme-default-discoveryCoral);
+        color: white;
+        font-size: 10px;
+        font-weight: bolder;
+        align-self: flex-end;
+        /* position: relative;
+        left: 4em;
+        top: 1em; */
+      }
+
+      .slot button:hover {
+        background-color: red;
+      }
+
+      .save-cancel-buttons {
+        margin-top: 2%;
       }
     `];
   }
@@ -65,15 +88,23 @@ export class haxcmsPartyUI extends DDD {
     addUser() {
       const inputValue = this.shadowRoot.querySelector('.input-text').value;
 
-      // Check if user already exists
+      // Cleanup input and assign to new variable
+      var userid = "";
+      userid = inputValue.toLowerCase();
+      userid = userid.trim();
 
-
-      // If they dont exist, continue
-      const user = {
-        userid: inputValue,
+      // Validate input / make sure there IS a input
+      if (userid == "") {
+        return;
       }
 
-      this.partyMembers = [...this.partyMembers, user];
+      // Check if user already exists
+      if (this.partyMembers.indexOf(userid) != -1) {
+        return;
+      }
+
+      // If they dont exist, continue
+      this.partyMembers = [...this.partyMembers, userid];
       console.log(this.partyMembers);
     }
 
@@ -110,6 +141,12 @@ export class haxcmsPartyUI extends DDD {
         );
     }
 
+    inputScrub(e) {
+      const inputValue = e.target.value;
+      const scrubValue = inputValue.replace(/[^a-zA-Z0-9]+$/g, "");
+      e.target.value = scrubValue.slice(0, 10);
+    }
+
     render() {
         return html`
             <confetti-container id="confetti">
@@ -117,20 +154,21 @@ export class haxcmsPartyUI extends DDD {
                   <h1 class="frame-title">Add Party Member</h1>
 
                   <div class="add-to-party-input">
-                    <input class="input-text" type="text" placeholder="abc123">
-                    <button @click="${this.addUser}">Add</button>
+                      <input class="input-text" type="text" placeholder="abc123" @input="${this.inputScrub}">
+                      <button type="submit" @click="${this.addUser}">Add</button>
                   </div>
 
                   <div class="party-members">
                       <div class="slot">
-                          <rpg-character seed="${this.ownerName}" walking></rpg-character>
+                          <button style="visibility: hidden;" class="delete-user">X</button>
+                          <rpg-character seed="${this.ownerName}"></rpg-character>
                           <p>${this.ownerName}</p>
                       </div>
-                      ${this.partyMembers.map((user) => html`
+                      ${this.partyMembers.map((userid) => html`
                         <div class="slot">
-                          <button class="delete-user" @click="${this.targetClicked}" seed="${user.userid}">X</button>
-                          <rpg-character seed=${user.userid} walking @click="${this.targetClicked}"></rpg-character>
-                          <p>${user.userid}</p>
+                          <button class="delete-user" @click="${this.targetClicked}" seed="${userid}">X</button>
+                          <rpg-character seed=${userid} @click="${this.targetClicked}"></rpg-character>
+                          <p>${userid}</p>
                         </div>
                     `)}
                   </div>
